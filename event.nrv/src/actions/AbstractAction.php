7af1b743b548992a93edc643d780c1bea898ad4d
toinou887 <rionde8u@etu.univ-lorraine.fr>
2023-10-25 15:07:38 +0200

    MAJ de AbstractAction

--- event.nrv/src/actions/AbstractAction.php
+++ event.nrv/src/actions/AbstractAction.php
@@ -9,9 +9,25 @@ use Slim\Psr7\Response;
 abstract class AbstractAction
 {
     protected ContainerInterface $container;
+
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
     }
+
+    public function exception(\Exception $e): array
+    {
+        return [
+            'message' => $e->getCode() . $e->getMessage(),
+            'exception' => [
+                'type' => get_class($e),
+                'message' => $e->getMessage(),
+                'code' => $e->getCode(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine()
+            ]
+        ];
+    }
+
     abstract public function __invoke(Request $request, Response $response, $args): Response;
 }
