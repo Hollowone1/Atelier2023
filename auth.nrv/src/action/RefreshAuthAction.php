d295da068fc0a76a780edf9db0640df1e1b1f00d
Antoine Stoecklin <astoeck@hotmail.fr>
2023-10-25 14:15:52 +0200

    base actions ok now filled with code

---
+++ auth.nrv/src/action/RefreshAuthAction.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace nrv\auth\api\app\actions;
+
+use nrv\auth\api\app\actions\AbstractAction;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
---
+++ auth.nrv/src/action/ValidateAuthAction.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace nrv\auth\api\app\actions;
+
+use nrv\auth\api\app\actions\AbstractAction;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
