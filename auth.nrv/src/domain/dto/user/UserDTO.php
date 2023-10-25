80348a61b64fb56ac35ce5e0842f17d91a4c9826
Antoine Stoecklin <astoeck@hotmail.fr>
2023-10-25 10:42:42 +0200

    userDTO redone

--- auth.nrv/src/domain/dto/user/UserDTO.php
+++ auth.nrv/src/domain/dto/user/UserDTO.php
@@ -9,12 +9,24 @@ class UserDTO {
     public string $email;
     public string $password;
     public int $role;
+    public $timestamps = false;
+    protected $fillable = ['password',
+        'actve',
+        'activation_token',
+        'activation_token_expiration_date',
+        'refresh_token',
+        'refresh_token_expiration_date',
+        'reset_passwd_token',
+        'reset_passwd_token_expiration_date',
+        'username'];
 
-    public function __construct(string $nom, string $prenom, string $email,string $password, int $role) {
+    public function __construct(string $nom, string $prenom, string $email,string $password, int $role, array $fillable) {
         $this->nom = $nom;
         $this->prenom = $prenom;
         $this->email = $email;
         $this->password = $password;
         $this->role = $role;
+        $this->fillable = $fillable;
+       
     }
 }
