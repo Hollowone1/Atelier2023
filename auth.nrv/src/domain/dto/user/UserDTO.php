65897090e47a478de32bfab0346a928684cf02f2
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2023-10-25 09:55:35 +0200

    move YML + UserDTO

--- auth.nrv/src/domain/dto/user/UserDTO.php
+++ auth.nrv/src/domain/dto/user/UserDTO.php
@@ -3,5 +3,15 @@
 namespace nrv\auth\api\domain\dto\user;
 
 class UserDTO {
+    public string $prenom;
+    public string $nom;
+    public string $email;
+    public int $role;
 
+    public function __construct(string $nom, string $prenom, string $email, int $role) {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->email = $email;
+        $this->role = $role;
+    }
 }
