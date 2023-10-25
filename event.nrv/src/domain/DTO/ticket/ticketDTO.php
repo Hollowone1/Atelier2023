deb669547e4338763ed00e2708a2961cdc8f0cff
Antoine Stoecklin <astoeck@hotmail.fr>
2023-10-25 09:36:01 +0200

    ticket dto done

--- event.nrv/src/domain/DTO/ticket/ticketDTO.php
+++ event.nrv/src/domain/DTO/ticket/ticketDTO.php
@@ -5,4 +5,14 @@ use nrv\event\api\domain\DTO;
 
 class ticketDTO extends DTO {
     
+    public string $qrCode;
+
+    public function __construct(string $qrCode)
+    {
+        $this->qrCode = $qrCode;
+    }
+
+    public function getQrCode(){
+        return $this->qrCode;
+    }
 }
