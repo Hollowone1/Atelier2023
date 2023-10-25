553671d3cc770a66f76508acccb39821070c6c40
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2023-10-25 09:41:03 +0200

    namespace DTO + arbo soiree + soireeDTO

---
+++ event.nrv/src/domain/DTO/soiree/soireeDTO.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace src\auth\api\domain\DTO;
+
+class soireeDTO {
+    public string $nom;
+    public string $theme;
+    public string $date;
+    public string $horaire;
+    public int $tarifNormal;
+    public int $tarifReduit;
+    public string $lieu;
+
+    public function __construct(string $nom, string $theme, string $date, string $horaire, int $tarifNormal, int $tarifReduit, string $lieu) {
+        $this->nom = $nom;
+        $this->theme = $theme;
+        $this->date = $date;
+        $this->horaire = $horaire;
+        $this->tarifNormal = $tarifNormal;
+        $this->tarifReduit = $tarifReduit;
+        $this->lieu = $lieu;
+    }
+}
