525d3193e29242c60be58e925ebe2fc2a4529f9c
Antoine Stoecklin <astoeck@hotmail.fr>
2023-10-25 09:43:08 +0200

    spectacle dto done

---
+++ event.nrv/src/domain/DTO/spectacle/spectacleDTO.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace nrv\event\api\domain\spectacle\DTO;
+use nrv\event\api\domain\DTO;
+
+class spectacleDTO extends DTO{
+
+        public string $titre;
+        public string $description;
+        public string $urlVideo;
+        public string $horaire;
+
+        public function __construct(string $titre,string $description,string $urlVideo,string $horaire)
+        {
+            $this->titre = $titre;
+            $this->description = $description;
+            $this->urlVideo = $urlVideo;
+            $this->horaire = $horaire;
+        }
+
+}
