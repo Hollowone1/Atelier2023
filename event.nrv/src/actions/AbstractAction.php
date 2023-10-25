6a8a9b163aa6cd6ccd8a3ea4a0d1e34f6e9e8305
Antoine Stoecklin <astoeck@hotmail.fr>
2023-10-25 14:21:01 +0200

    add actions empty filled

---
+++ event.nrv/src/actions/AbstractAction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace nrv\event\api\app\actions;
+
+use Psr\Container\ContainerInterface;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+abstract class AbstractAction
+{
+    protected ContainerInterface $container;
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+    abstract public function __invoke(Request $request, Response $response, $args): Response;
+}
