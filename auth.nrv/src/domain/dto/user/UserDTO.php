29a0cb167a012a70dfe7a953ef682b7722291bd7
Antoine Stoecklin <astoeck@hotmail.fr>
2023-10-25 10:30:05 +0200

    user dto changed

--- auth.nrv/src/domain/dto/user/UserDTO.php
+++ auth.nrv/src/domain/dto/user/UserDTO.php
@@ -7,12 +7,14 @@ class UserDTO {
     public string $prenom;
     public string $nom;
     public string $email;
+    public string $password;
     public int $role;
 
-    public function __construct(string $nom, string $prenom, string $email, int $role) {
+    public function __construct(string $nom, string $prenom, string $email,string $password, int $role) {
         $this->nom = $nom;
         $this->prenom = $prenom;
         $this->email = $email;
+        $this->password = $password;
         $this->role = $role;
     }
 }
