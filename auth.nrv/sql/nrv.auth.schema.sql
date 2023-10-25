a916edc8ac992b1ed8dc0979c928af02aa891710
BJulOs <julesbresson88600@gmail.com>
2023-10-25 10:15:35 +0200

    maj bdd relative aux tokens

--- auth.nrv/sql/nrv.auth.schema.sql
+++ auth.nrv/sql/nrv.auth.schema.sql
@@ -8,12 +8,19 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE Utilisateur
 (
-    idUtilisateur   INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    prenom          VARCHAR(255)       NOT NULL,
-    nom             VARCHAR(255)       NOT NULL,
-    email           VARCHAR(255)       NOT NULL,
-    password        VARCHAR(255)       NOT NULL,
-    dateInscription DATE               NOT NULL,
-    role            INT(11)            NOT NULL
+    idUtilisateur                      INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    prenom                             VARCHAR(255)       NOT NULL,
+    nom                                VARCHAR(255)       NOT NULL,
+    email                              VARCHAR(255)       NOT NULL,
+    password                           VARCHAR(255)       NOT NULL,
+    dateInscription                    DATE               NOT NULL,
+    active                             TINYINT(4)         NOT NULL DEFAULT 0,
+    activation_token                   VARCHAR(64)                 DEFAULT NULL,
+    activation_token_expiration_date   TIMESTAMP          NULL     DEFAULT NULL,
+    refresh_token                      VARCHAR(64)                 DEFAULT NULL,
+    refresh_token_expiration_date      TIMESTAMP          NULL     DEFAULT NULL,
+    reset_passwd_token                 VARCHAR(64)                 DEFAULT NULL,
+    reset_passwd_token_expiration_date TIMESTAMP          NULL     DEFAULT NULL,
+    role                               INT(11)            NOT NULL
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
