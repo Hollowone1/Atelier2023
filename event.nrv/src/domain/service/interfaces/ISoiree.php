3c3701077d050285fcc0d9411ecf65036c8106c9
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2023-10-26 11:45:57 +0200

    soirée service

--- event.nrv/src/domain/service/interfaces/ISoiree.php
+++ event.nrv/src/domain/service/interfaces/ISoiree.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace nrv\event\api\domain\service\interfaces;
+use nrv\event\api\domain\DTO\event\soireeDTO;
+
 
 interface ISoiree
 {
-    public function creerSoiree(string $nom, string $theme, string $date, string $horaire, int $tarifNormal, int $tarifReduit, string $lieu): array;
-    public function recupSoiree(string $id): array;
+    public function creerSoiree(SoireeDTO $soireeDTO): SoireeDTO;
+    public function recupSoiree(string $id): SoireeDTO;
     public function supprSoiree(string $id): array;
     public function recupToutesLesSoirees(): array;
+    public function recupSpectacles(string $idSoiree) : array;
 }
