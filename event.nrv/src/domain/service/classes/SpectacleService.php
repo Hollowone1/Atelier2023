4ad3ca6ba027d68b7aa2a4c27057819298d13dba
toinou887 <rionde8u@etu.univ-lorraine.fr>
2023-10-26 12:02:30 +0200

    Ajout de l'interface ISpectacle + ajout du service spectacle

---
+++ event.nrv/src/domain/service/classes/SpectacleService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace nrv\event\api\domain\service\classes;
+
+use Exception;
+use nrv\event\api\domain\DTO\event\spectacleDTO;
+use nrv\event\api\domain\entities\event\Spectacle;
+use nrv\event\api\domain\service\interfaces\ISpectacle;
+
+class SpectacleService implements ISpectacle
+{
+
+    public function creerSpectacle(SpectacleDTO $spectacleDTO): SpectacleDTO
+    {
+        $spectacleEntity = new Spectacle();
+        $spectacleId = Uid::uuid4()->toString();
+        $spectacleEntity->idSpectacle = $spectacleId;
+        $spectacleEntity->titre = $spectacleDTO->titre;
+        $spectacleEntity->description = $spectacleDTO->description;
+        $spectacleEntity->urlVideo = $spectacleDTO->urlVideo;
+        $spectacleEntity->horaire = $spectacleDTO->horaire;
+        return new spectacleDTO($spectacleEntity->titre, $spectacleEntity->description, $spectacleEntity->urlVideo, $spectacleEntity->horaire);
+    }
+
+    public function recupSpectacle(string $id): SpectacleDTO
+    {
+        $billetEntity = Spectacle::find($id);
+        if ($billetEntity == null) {
+            throw new Exception("Billet introuvable");
+        }
+        return $billetEntity->toDTO();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function supprSpectacle(string $id): array
+    {
+        $billetEntity = $this->recupSpectacle($id);
+        $billetEntity->delete();
+        return ["message" => "Spectacle supprimé"];
+    }
+
+    public function recupTousLesSpectacles(): array
+    {
+        $spectacles = Spectacle::all();
+        $spectaclesDTO = [];
+        foreach ($spectacles as $spectacle) {
+            $spectaclesDTO[] = $spectacle->toDTO();
+        }
+        return $spectaclesDTO;
+    }
+}
