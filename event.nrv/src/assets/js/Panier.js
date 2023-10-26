97e099edbce28dde42800be10c4969b494d88889
tamourgh1u <grindmoney19@gmail.com>
2023-10-26 15:32:32 +0200

    correction Panier.js / Commande.js

--- event.nrv/src/assets/js/Panier.js
+++ event.nrv/src/assets/js/Panier.js
@@ -1,59 +1,60 @@
-import {apiNRVAuth} from 'Config.js';
-//panier.html, répétition fetch des éléments du panier:
+  import { apiNRVAuth } from 'Config.js';
 
+// Logique pour récupérer les données du panier
+const nombreDeBilletsAchetes = 2;
+const panier = document.querySelector(".tab"); // Sélectionnez le conteneur de panier
+
+// Assurez-vous d'ajuster l'URL de l'API en conséquence
 fetch(`${apiNRVAuth}panier`)
   .then(response => response.json())
   .then(data => {
     // Traitez les données de l'API ici
     // Parcourez les données pour chaque élément du panier
-    // Créez une nouvelle ligne de panier pour chaque élément du panier
-    data.forEach(element => {
-      // Créez une nouvelle ligne de panier
-      const newRow = document.createElement('div');
-      newRow.className = 'row content';
+    for (let i = 0; i < nombreDeBilletsAchetes; i++) {
+      // Créez une nouvelle ligne de panier pour chaque élément
+      const panierItemDiv = document.createElement("div");
+      panierItemDiv.className = "row content";
 
-      // Créez la case à cocher
-      const checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.name = 'item';
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.name = "item";
 
-      // Créez les éléments de la ligne de panier
-      const lieu = document.createElement('div');
-      lieu.className = 'panier-lieu';
-      lieu.textContent = element.lieu; 
+      const lieu = document.createElement("div");
+      lieu.className = "panier-lieu";
+      lieu.textContent = data[i].lieu; // Assurez-vous que la structure des données de l'API correspond à cela
 
-      const soiree = document.createElement('div');
-      soiree.className = 'panier-soiree';
-      soiree.textContent = element.soiree;
+      const soiree = document.createElement("div");
+      soiree.className = "panier-soiree";
+      soiree.textContent = data[i].soiree; // Assurez-vous que la structure des données de l'API correspond à cela
 
-      const type = document.createElement('div');
-      type.className = 'panier-type';
-      type.textContent = element.type;
+      const type = document.createElement("div");
+      type.className = "panier-type";
+      type.textContent = data[i].type; // Assurez-vous que la structure des données de l'API correspond à cela
 
-      const prix = document.createElement('div');
-      prix.className = 'panier-prix';
-      prix.textContent = element.prix; 
+      const prix = document.createElement("div");
+      prix.className = "panier-prix";
+      prix.textContent = data[i].prix; // Assurez-vous que la structure des données de l'API correspond à cela
 
-      const quantite = document.createElement('div');
-      quantite.className = 'panier-quantite';
-      quantite.textContent = element.quantite;
+      const quantite = document.createElement("div");
+      quantite.className = "panier-quantite";
+      quantite.textContent = data[i].quantite; // Assurez-vous que la structure des données de l'API correspond à cela
 
-      // Ajoutez la case à cocher et les éléments de la ligne au nouveau conteneur de ligne
-      newRow.appendChild(checkbox);
-      newRow.appendChild(lieu);
-      newRow.appendChild(soiree);
-      newRow.appendChild(type);
-      newRow.appendChild(prix);
-      newRow.appendChild(quantite);
+      // Ajoutez les éléments à la ligne de panier
+      panierItemDiv.appendChild(checkbox);
+      panierItemDiv.appendChild(lieu);
+      panierItemDiv.appendChild(soiree);
+      panierItemDiv.appendChild(type);
+      panierItemDiv.appendChild(prix);
+      panierItemDiv.appendChild(quantite);
 
       // Ajoutez la nouvelle ligne de panier au conteneur du panier
-      const panier = document.getElementById('panier');
-      panier.appendChild(newRow);
-    });
+      panier.appendChild(panierItemDiv);
+    }
   })
   .catch(error => {
-    console.error('Erreur lors de la récupération des éléments de panier :', error);
+    console.error('Erreur lors de la récupération des données du panier :', error);
   });
+
 
 
 //panier.html, suppression des éléments du panier:
