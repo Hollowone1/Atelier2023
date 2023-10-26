717e05f4beaf2304e8a0fa4afe20c5c98a6d8ea0
Antoine Stoecklin <astoeck@hotmail.fr>
2023-10-26 09:47:53 +0200

    user created, added role to user

--- event.nrv/src/model/User.php
+++ event.nrv/src/model/User.php
@@ -6,23 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    /*
-     * email	varchar(128)
-password	varchar(256)
-active	tinyint(4) [0]
-activation_token	varchar(64) NULL
-activation_token_expiration_date	timestamp NULL
-refresh_token	varchar(64) NULL
-refresh_token_expiration_date	timestamp NULL
-reset_passwd_token	varchar(64) NULL
-reset_passwd_token_expiration_date	timestamp NULL
-username	varchar(64) NULL
-     */
     protected $connection = 'auth';
     protected $table = 'users';
     protected $primaryKey = 'email';
     protected $keyType= 'string';
     public $timestamps = false;
+    public $role = [1,2,3];
     protected $fillable = ['email', 'password', 'active', 'activation_token', 'activation_token_expiration_date', 'refresh_token', 'refresh_token_expiration_date', 'reset_passwd_token', 'reset_passwd_token_expiration_date', 'username'];
 
 
