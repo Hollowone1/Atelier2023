d2a3b341a863b4a3515cf9b37a995e06e595c105
toinou887 <rionde8u@etu.univ-lorraine.fr>
2023-10-26 13:40:27 +0200

    Ajout des routes + ajout de 2 actions : HomeAction + AccederSpectacleAction

--- event.nrv/config/routes.php
+++ event.nrv/config/routes.php
@@ -3,3 +3,21 @@ declare(strict_types=1);
 
 use Psr\Http\Message\ServeurRequestInterface as Request;
 use Psr\Http\Message\ResponseFactoryInterface as Response;
+use nrv\event\api\app\actions\get\AccederSpectacleAction;
+use Slim\App;
+
+return function (App $app):void {
+    $app->get('/', \nrv\event\api\app\actions\HomeAction::class)
+        ->setName('home');
+    $app->get('/spectacles[/]', \nrv\event\api\app\actions\get\AccederSpectaclesAction::class)
+        ->setName('spectacles');
+    $app->get('/spectacles/{idSpectacle}[/]', AccederSpectacleAction::class)
+        ->setName('spectacle');
+    $app->post('/spectacles/{idSpectacle}[/]', \nrv\event\api\app\actions\post\CreerSpectacleAction::class)
+        ->setName('creer_spectacle');
+    $app->get('/billets/{idBillet}[/]', \nrv\event\api\app\actions\get\AccederBilletAction::class)
+        ->setName('billet');
+    $app->post('/billets/{idBillet}[/]', \nrv\event\api\app\actions\post\ValiderBilletAction::class)
+        ->setName('valider_billet');
+
+};
