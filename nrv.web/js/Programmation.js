84b37706e29805b875080417f0ad65ea91a89925
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2023-10-27 12:12:35 +0200

    prog OK ?

--- nrv.web/js/Programmation.js
+++ nrv.web/js/Programmation.js
@@ -8,6 +8,78 @@ import {apiNRVEvent} from './Config.js';
 //TODO : filtre lieu (avec id)
 //TODO : filtre date (avec valeur input)
 
+
+function filtreDate(date) {
+  const inputDate = document.getElementById("input-filtre-date");
+  const dateValeur = inputDate.value;
+  console.log("Le lieu sélectionné est : ", dateValeur);
+
+  fetch(`${apiNRVEvent}date`)
+      .then(response => response.json())
+      .then(data => {
+        const container = document.getElementsByClassName('list');
+
+        //l'action renvoie un tableau avec un DTO pr chaque soirée
+        data.forEach(soiree => {
+          const sectionSoiree = document.createElement('section');
+          sectionSoiree.className = 'soiree';
+
+          const soireeContainer = document.createElement('div');
+          soireeContainer.className = 'soiree-container';
+
+          const imageSoiree = document.createElement('img');
+          imageSoiree.className = 'soiree-image';
+          imageSoiree.src = soiree.image[0];
+          imageSoiree.alt = 'image de la soirée';
+          soireeContainer.appendChild(imageSoiree);
+
+          const infosSoiree = document.createElement('div');
+          infosSoiree.className = 'soiree-infos';
+
+          const dateSoiree = document.createElement('p');
+          dateSoiree.className = 'soiree-infos-date';
+          dateSoiree.textContent = `Date de la soirée: ${soiree.date}`;
+          infosSoiree.appendChild(dateSoiree);
+
+          const titreSoiree = document.createElement('h2');
+          titreSoiree.className = 'soiree-infos-titre';
+          titreSoiree.textContent = soiree.nom;
+          infosSoiree.appendChild(titreSoiree);
+
+          const lieuSoiree = document.createElement('p');
+          lieuSoiree.className = 'soiree-infos-lieu';
+          lieuSoiree.textContent = `Lieu de la soirée: ${soiree.lieu}`;
+          infosSoiree.appendChild(lieuSoiree);
+
+          soireeContainer.appendChild(infosSoiree);
+          sectionSoiree.appendChild(soireeContainer);
+
+          // Créez les boutons pour la soirée
+          const buttonsSoiree = document.createElement('div');
+          buttonsSoiree.className = 'soiree-buttons';
+
+          const enSavoirPlusButton = document.createElement('button');
+          enSavoirPlusButton.className = 'soiree-buttons-1';
+          enSavoirPlusButton.textContent = 'En savoir +';
+          buttonsSoiree.appendChild(enSavoirPlusButton);
+
+          const reserverButton = document.createElement('button');
+          reserverButton.className = 'soiree-buttons-2';
+          reserverButton.textContent = 'Réserver';
+          buttonsSoiree.appendChild(reserverButton);
+
+          sectionSoiree.appendChild(buttonsSoiree);
+
+          // Ajoutez la nouvelle section de soirée au conteneur des soirées
+          container.appendChild(sectionSoiree);
+        });
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération des dates :', error);
+      });
+
+}
+
 fetch(`${apiNRVEvent}boutons_theme`)
     .then(response => response.json())
     .then(data => {
@@ -36,10 +108,136 @@ function filtreGenre(genre) {
   fetch(`${apiNRVEvent}theme`)
       .then(response => response.json())
       .then(data => {
+        const container = document.getElementsByClassName('list');
 
+        //l'action renvoie un tableau avec un DTO pr chaque soirée
+        data.forEach(soiree => {
+          const sectionSoiree = document.createElement('section');
+          sectionSoiree.className = 'soiree';
+
+          const soireeContainer = document.createElement('div');
+          soireeContainer.className = 'soiree-container';
+
+          const imageSoiree = document.createElement('img');
+          imageSoiree.className = 'soiree-image';
+          imageSoiree.src = soiree.image[0];
+          imageSoiree.alt = 'image de la soirée';
+          soireeContainer.appendChild(imageSoiree);
+
+          const infosSoiree = document.createElement('div');
+          infosSoiree.className = 'soiree-infos';
+
+          const dateSoiree = document.createElement('p');
+          dateSoiree.className = 'soiree-infos-date';
+          dateSoiree.textContent = `Date de la soirée: ${soiree.date}`;
+          infosSoiree.appendChild(dateSoiree);
+
+          const titreSoiree = document.createElement('h2');
+          titreSoiree.className = 'soiree-infos-titre';
+          titreSoiree.textContent = soiree.nom;
+          infosSoiree.appendChild(titreSoiree);
+
+          const lieuSoiree = document.createElement('p');
+          lieuSoiree.className = 'soiree-infos-lieu';
+          lieuSoiree.textContent = `Lieu de la soirée: ${soiree.lieu}`;
+          infosSoiree.appendChild(lieuSoiree);
+
+          soireeContainer.appendChild(infosSoiree);
+          sectionSoiree.appendChild(soireeContainer);
+
+          // Créez les boutons pour la soirée
+          const buttonsSoiree = document.createElement('div');
+          buttonsSoiree.className = 'soiree-buttons';
+
+          const enSavoirPlusButton = document.createElement('button');
+          enSavoirPlusButton.className = 'soiree-buttons-1';
+          enSavoirPlusButton.textContent = 'En savoir +';
+          buttonsSoiree.appendChild(enSavoirPlusButton);
+
+          const reserverButton = document.createElement('button');
+          reserverButton.className = 'soiree-buttons-2';
+          reserverButton.textContent = 'Réserver';
+          buttonsSoiree.appendChild(reserverButton);
+
+          sectionSoiree.appendChild(buttonsSoiree);
+
+          // Ajoutez la nouvelle section de soirée au conteneur des soirées
+          container.appendChild(sectionSoiree);
+        });
       })
       .catch(error => {
         console.error('Erreur lors de la récupération des genres musicaux :', error);
+      });
+
+}
+
+function filtreLieu(lieu) {
+  const boutonLieu = document.getElementById(lieu);
+  const lieuValeur = boutonLieu.value;
+  console.log("Le lieu sélectionné est : ", lieuValeur);
+
+  fetch(`${apiNRVEvent}lieu`)
+      .then(response => response.json())
+      .then(data => {
+        const container = document.getElementsByClassName('list');
+
+        //l'action renvoie un tableau avec un DTO pr chaque soirée
+        data.forEach(soiree => {
+          const sectionSoiree = document.createElement('section');
+          sectionSoiree.className = 'soiree';
+
+          const soireeContainer = document.createElement('div');
+          soireeContainer.className = 'soiree-container';
+
+          const imageSoiree = document.createElement('img');
+          imageSoiree.className = 'soiree-image';
+          imageSoiree.src = soiree.image[0];
+          imageSoiree.alt = 'image de la soirée';
+          soireeContainer.appendChild(imageSoiree);
+
+          const infosSoiree = document.createElement('div');
+          infosSoiree.className = 'soiree-infos';
+
+          const dateSoiree = document.createElement('p');
+          dateSoiree.className = 'soiree-infos-date';
+          dateSoiree.textContent = `Date de la soirée: ${soiree.date}`;
+          infosSoiree.appendChild(dateSoiree);
+
+          const titreSoiree = document.createElement('h2');
+          titreSoiree.className = 'soiree-infos-titre';
+          titreSoiree.textContent = soiree.nom;
+          infosSoiree.appendChild(titreSoiree);
+
+          const lieuSoiree = document.createElement('p');
+          lieuSoiree.className = 'soiree-infos-lieu';
+          lieuSoiree.textContent = `Lieu de la soirée: ${soiree.lieu}`;
+          infosSoiree.appendChild(lieuSoiree);
+
+          soireeContainer.appendChild(infosSoiree);
+          sectionSoiree.appendChild(soireeContainer);
+
+          // Créez les boutons pour la soirée
+          const buttonsSoiree = document.createElement('div');
+          buttonsSoiree.className = 'soiree-buttons';
+
+          const enSavoirPlusButton = document.createElement('button');
+          enSavoirPlusButton.className = 'soiree-buttons-1';
+          enSavoirPlusButton.textContent = 'En savoir +';
+          buttonsSoiree.appendChild(enSavoirPlusButton);
+
+          const reserverButton = document.createElement('button');
+          reserverButton.className = 'soiree-buttons-2';
+          reserverButton.textContent = 'Réserver';
+          buttonsSoiree.appendChild(reserverButton);
+
+          sectionSoiree.appendChild(buttonsSoiree);
+
+          // Ajoutez la nouvelle section de soirée au conteneur des soirées
+          container.appendChild(sectionSoiree);
+        });
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération des lieux :', error);
       });
 
 }
@@ -63,11 +261,7 @@ fetch(`${apiNRVEvent}boutons_lieu`)
       console.error('Erreur lors de la récupération des genres musicaux :', error);
     });
 
-
-
-
   //programmation.html, répéter les soirées:
-
   fetch(`${apiNRVEvent}soirees`)
   .then(response => response.json())
   .then(data => {
@@ -83,11 +277,10 @@ fetch(`${apiNRVEvent}boutons_lieu`)
 
       const soireeContainer = document.createElement('div');
       soireeContainer.className = 'soiree-container';
-      sectionSoiree.appendChild(soireeContainer);
 
       const imageSoiree = document.createElement('img');
       imageSoiree.className = 'soiree-image';
-      imageSoiree.src = soiree.image;
+      imageSoiree.src = soiree.image[0];
       imageSoiree.alt = 'image de la soirée';
       soireeContainer.appendChild(imageSoiree);
 
@@ -101,7 +294,7 @@ fetch(`${apiNRVEvent}boutons_lieu`)
 
       const titreSoiree = document.createElement('h2');
       titreSoiree.className = 'soiree-infos-titre';
-      titreSoiree.textContent = soiree.titre;
+      titreSoiree.textContent = soiree.nom;
       infosSoiree.appendChild(titreSoiree);
 
       const lieuSoiree = document.createElement('p');
@@ -110,7 +303,6 @@ fetch(`${apiNRVEvent}boutons_lieu`)
       infosSoiree.appendChild(lieuSoiree);
 
       soireeContainer.appendChild(infosSoiree);
-
       sectionSoiree.appendChild(soireeContainer);
 
       // Créez les boutons pour la soirée
