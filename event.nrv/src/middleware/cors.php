c92909473c2976dc4427fe8d797ae07c4be2fb3e
Antoine Stoecklin <astoeck@hotmail.fr>
2023-10-27 12:20:15 +0200

    cors structure may working

--- event.nrv/src/middleware/cors.php
+++ event.nrv/src/middleware/cors.php
@@ -1,18 +1,28 @@
 <?php
 namespace nrv\event\api\middleware;
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface;
+
 
 class CorsMiddleware
 {
-    public function __invoke($request, $response, $next)
+    public function __invoke(Request $request, Response $response)
     {
-        $response = $next($request, $response);
+        $app = AppFactory::create();
 
-        $response = $response
-            ->withHeader('Access-Control-Allow-Origin', 'http://docketu.iutnc.univ-lorraine.fr:16584/')//chercher les headers origin dans la requête
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-            ->withHeader('Access-Control-Allow-Credentials', 'true');
-
-        return $response;
+        $app->options('/{routes:.+}', function ($request, $response, $args) {
+            return $response;
+        });
+        
+        $app->add(function ($request, $handler) {
+            $response = $handler->handle($request);
+            $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+                return $response
+                    ->withHeader('Access-Control-Allow-Origin', 'https://webetu.iutnc.univ-lorraine.fr/www/rionde8u/')
+                    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+                    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+        });
     }
 }
