99355ca920ec797bb9650e7083c731854dc24cdf
tamourgh1u <grindmoney19@gmail.com>
2023-10-27 12:29:45 +0200

    menu.js

--- nrv.web/js/menu.js
+++ nrv.web/js/menu.js
@@ -1,16 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const burgerMenu = document.getElementById('burger-menu');
-    const mobileNav = document.querySelector('.mobile-nav');
-  
-    if (burgerMenu) {
-      burgerMenu.addEventListener('click', function () {
-        console.log('cest ok');
-        mobileNav.classList.toggle('active'); });
-    }
-  });
-  
-
-  function myFunction() {
+ function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
       x.style.display = "none";
