c87f5570953fd82904a7097a8f93b8df710d9c8a
Antoine Stoecklin <astoeck@hotmail.fr>
2023-10-27 10:19:19 +0200

    push forms for authentification

---
+++ auth.nrv/src/domain/service/forms/form-inscription.php
@@ -0,0 +1,54 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $errors = array();
+
+    if (isset($_POST["prenom"])) {
+        $prenom = $_POST["prenom"];
+        if (empty($prenom)) {
+            $errors[] = "Le prénom est obligatoire.";
+        }
+    } else {
+        $errors[] = "Le prénom n'a pas été soumis.";
+    }
+
+    if (isset($_POST["nom"])) {
+        $nom = $_POST["nom"];
+        if (empty($nom)) {
+            $errors[] = "Le nom est obligatoire.";
+        }
+    } else {
+        $errors[] = "Le nom n'a pas été soumis.";
+    }
+
+    if (isset($_POST["email"])) {
+        $email = $_POST["email"];
+        if (empty($email)) {
+            $errors[] = "L'adresse e-mail est obligatoire.";
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors[] = "L'adresse e-mail n'est pas valide.";
+        }
+    } else {
+        $errors[] = "L'adresse e-mail n'a pas été soumise.";
+    }
+
+    if (isset($_POST["motdepasse"])) {
+        $motdepasse = $_POST["motdepasse"];
+        if (empty($motdepasse)) {
+            $errors[] = "Le mot de passe est obligatoire.";
+        } 
+        } 
+        else {
+        $errors[] = "Le mot de passe n'a pas été soumis.";
+    }
+
+    if (empty($errors)) {
+        header("Location: accueil.html");
+        exit();
+    } else {
+        // Il y a des erreurs, affichez-les à l'utilisateur
+        foreach ($errors as $error) {
+            echo $error . "<br>";
+        }
+    }
+}
+?>
