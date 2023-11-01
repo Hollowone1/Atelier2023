7de2edb54f32d652b0bee6c845a8ba117de881db
Antoine Stoecklin <astoeck@hotmail.fr>
2023-11-01 09:18:18 +0100

    panierDTO done

--- auth.nrv/src/domain/dto/panier/panierDTO.php
+++ auth.nrv/src/domain/dto/panier/panierDTO.php
@@ -2,6 +2,36 @@
 
 namespace nrv\auth\api\domain\dto\panier;
 
-class panierDTO {
 
+class PanierDTO
+{
+
+    private string $lieu;
+    private string $soiree;
+    private string $type;
+    private string $prix;
+    private string $quantite;
+    
+
+    public function __construct(string $lieu, string $soiree, string $type, float $prix, int $quantite)
+    {
+        $this->lieu = $lieu;
+        $this->soiree = $soiree;
+        $this->type = $type;
+        $this->prix = $prix;
+        $this->quantite = $quantite;
+        
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'lieu' => $this->lieu,
+            'soiree' => $this->soiree,
+            'type' => $this->type,
+            'prix' => $this->prix,
+            'quantite' => $this->quantite,
+            
+        ];
+    }
 }
